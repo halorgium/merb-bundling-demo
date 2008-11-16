213bdcfc2a4a945171e1ae6d7bd7ece8e8a34b5f
Tim Carey-Smith <tim@spork.in>
2008-11-15 21:44:04 -0800

    Change out the bundling to be more awesome

--- config/init.rb
+++ config/init.rb
@@ -1,7 +1,6 @@
 # Go to http://wiki.merbivore.com/pages/init-rb
- 
-# Specify a specific version of a dependency
-# dependency "RedCloth", "> 3.0"
+require File.dirname(__FILE__) + "/rubundler"
+Rubundler.new.setup_requirements
 
 #  use_orm :none
 use_test :rspec
